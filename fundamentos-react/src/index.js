17e00326e257c0acbda5b18a2477a5621106c573
Adriel Modollo <adrielmodollo@outlook.com>
2021-11-08 23:12:28 -0300

    Import CSS

--- fundamentos-react/src/index.js
+++ fundamentos-react/src/index.js
@@ -1,13 +1,13 @@
+import './index.css'
 import ReactDOM from 'react-dom' //Renderiza uma String normal na tela
 import React from 'react' //Faz a conversão de html para Javascript
 
 const tag = <strong> Olá React!!! </strong>
 
-
 const el = document.getElementById('root') // Recupera o id root em nosso index.js dentro da div
 ReactDOM.render(
     <div> 
-       { tag }
+       { tag } 
     </div>
     , el
 )
