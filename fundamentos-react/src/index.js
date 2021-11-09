7163ff2befca32f970552a4b2534a704fc1f3f49
Adriel Modollo <adrielmodollo@outlook.com>
2021-11-08 23:19:23 -0300

    import function react

--- fundamentos-react/src/index.js
+++ fundamentos-react/src/index.js
@@ -1,13 +1,12 @@
-import './index.css'
+import './index.css' //Para encontrar o arquivo no caminho relativo informado.
 import ReactDOM from 'react-dom' //Renderiza uma String normal na tela
 import React from 'react' //Faz a conversão de html para Javascript
-
-const tag = <strong> Olá React!!! </strong>
+import Primeiro from './components/basicos/Primeiro'
 
 const el = document.getElementById('root') // Recupera o id root em nosso index.js dentro da div
 ReactDOM.render(
     <div> 
-       { tag } 
+       <Primeiro></Primeiro>
     </div>
     , el
 )
