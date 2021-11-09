f99efe7baab2c9a4d1cd4f95a0be5298a183ffd2
Adriel Modollo <adrielmodollo@outlook.com>
2021-11-08 23:57:06 -0300

    Possíveis erros de envolvimento de elementos

--- fundamentos-react/src/components/basicos/fragmento.jsx
+++ fundamentos-react/src/components/basicos/fragmento.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 
 export default function Fragmento(props){
     return (
-        <div>
+        //Quando envolvido com div solucionamos o problema, mas quando passamos o elemento sem envolver o conteudo ocorre o erro
+        //React.Fragment outra forma de soluciona o problema
+        // <> outra solução, porém não conseguimos passar atributos nesta sintaxe reduzida
+        <>
             <h2>Fragmento</h2>
             <p>Cuidado com esse erro!</p>
-        </div>
+        </>
     )
 }
