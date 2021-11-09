41b739689b15c67d2f97bbe6fab3e3fc0aa8edcc
Adriel Modollo <adrielmodollo@outlook.com>
2021-11-08 23:36:40 -0300

    Function com Parametros
    Props = propriedades

---
+++ fundamentos-react/src/components/basicos/ComParametro.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function ComParametro(props){
+    //const sub = props.subtitulo
+    
+    return (
+        <div>
+            <h2>{props.titulo}</h2>
+            <h3>{props.subtitulo}</h3>
+        </div>
+    )
+}
--- fundamentos-react/src/index.js
+++ fundamentos-react/src/index.js
@@ -1,12 +1,17 @@
 import './index.css' //Para encontrar o arquivo no caminho relativo informado.
 import ReactDOM from 'react-dom' //Renderiza uma String normal na tela
 import React from 'react' //Faz a conversão de html para Javascript
+
 import Primeiro from './components/basicos/Primeiro'
+import ComParametro from './components/basicos/ComParametro'
 
 const el = document.getElementById('root') // Recupera o id root em nosso index.js dentro da div
 ReactDOM.render(
-    <div> 
+    <div id="app"> 
        <Primeiro></Primeiro>
+       <ComParametro 
+            titulo="Segundo Componente" 
+            subtitulo="Muito massa!"/>
     </div>
     , el
 )
