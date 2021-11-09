0a235db0203cb4d6c19c4f4e408eac1f3e33c4c1
Adriel Modollo <adrielmodollo@outlook.com>
2021-11-08 23:09:22 -0300

    Outra forma de trabalhar com React

--- fundamentos-react/src/index.js
+++ fundamentos-react/src/index.js
@@ -1,10 +1,13 @@
 import ReactDOM from 'react-dom' //Renderiza uma String normal na tela
 import React from 'react' //Faz a conversão de html para Javascript
 
+const tag = <strong> Olá React!!! </strong>
+
+
 const el = document.getElementById('root') // Recupera o id root em nosso index.js dentro da div
 ReactDOM.render(
     <div> 
-        <strong> Olá React!!! </strong>
+       { tag }
     </div>
     , el
 )
