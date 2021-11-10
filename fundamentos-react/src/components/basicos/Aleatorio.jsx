38778f8385ca04fdfbe5341c4aec502875755e63
Adriel Modollo <adrielmodollo@outlook.com>
2021-11-09 23:14:57 -0300

    Arrow

--- fundamentos-react/src/components/basicos/Aleatorio.jsx
+++ fundamentos-react/src/components/basicos/Aleatorio.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 
 export default props => {
-    const aleatorio = parseInt(Math.random() * (props.max - props.min)) + props.min;
+    const {min, max} = props;
+
+    const aleatorio = parseInt(Math.random() * (max - min)) + min;
     return (
         <div>
             <h2> Valor aleatório</h2>
-            <p><strong>Valor mínimo</strong> {props.min}</p>
-            <p><strong>Valor máximo</strong> {props.max}</p>
+            <p><strong>Valor mínimo</strong> {min}</p>
+            <p><strong>Valor máximo</strong> {max}</p>
             <p><strong>Valor Escolhido</strong> {aleatorio}</p>
         </div>
     );
