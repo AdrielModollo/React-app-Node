6c127c1ba3bad24663062e8573f7413a64d6d725
Adriel Modollo <adrielmodollo@outlook.com>
2021-11-09 22:40:11 -0300

    Formatação

--- fundamentos-react/src/App.jsx
+++ fundamentos-react/src/App.jsx
@@ -4,16 +4,15 @@ import Primeiro from './components/basicos/Primeiro'
 import ComParametro from './components/basicos/ComParametro'
 import Fragmento from './components/basicos/Fragmento'
 
+//Alt + Shift + F <=formata
 
-
-export default _ => 
-        <div id="app"> 
-            <h1> Fundamentos React </h1>
-            <Fragmento />
-            <ComParametro 
-                    titulo="Situação do aluno" 
-                    aluno="Adriel" nota={8.7}
-            />
-            <Primeiro></Primeiro>
-        </div>
- 
+export default _ =>
+    <div id="app">
+        <h1> Fundamentos React </h1>
+        <Fragmento />
+        <ComParametro
+            titulo="Situação do aluno"
+            aluno="Adriel" nota={8.7}
+        />
+        <Primeiro></Primeiro>
+    </div>
